46b378129b7f8934812b75519e10d2f202501d16
Kril Allen <15674734251@163.com>
2017-08-29 14:54:18 +0800

    Update routing.module.ts

--- src/app/routing.module.ts
+++ src/app/routing.module.ts
@@ -4,13 +4,17 @@ import { RouterModule, Routes } from '@angular/router';
 import { DatasComponent } from './datas/datas.component';
 import { ParkingsComponent } from './parkings/parkings.component';
 import { MapsComponent } from './maps/maps.component';
+import { InfosComponent } from './infos/infos.component';
 import { Map1sComponent } from './map1s/map1s.component';
+import { HomesComponent } from './homes/homes.component';
+import { StatsComponent } from './stats/stats.component';
+import { SlidersComponent } from './sliders/sliders.component';
 import { AppComponent } from './app.component';
 
 //将DatasComponent设置为初始页面
 
 const routes: Routes = [
-  { path: '', component: MapsComponent },
+  { path: '', component: HomesComponent },
 ];
 
 @NgModule({

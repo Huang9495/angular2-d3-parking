dcb3a2ac167dfbb2491ba7b3dbf783f98486393b
Kril Allen <15674734251@163.com>
2017-08-29 14:58:34 +0800

    Add files via upload

--- src/app/maps/maps.component.ts
+++ src/app/maps/maps.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Inject,ElementRef, AfterViewInit,ViewEncapsulation} from '@angular/core';
+import { Component, Output,OnInit, Inject,ElementRef, AfterViewInit,ViewEncapsulation,EventEmitter} from '@angular/core';
 import { D3Service, D3, Selection } from 'd3-ng2-service'; 
 
 import * as L from 'leaflet';
@@ -17,6 +17,8 @@ import { FormGroup, FormControl, Validators, FormBuilder } from '@angular/forms'
 import { MapService } from '../services/map.service';
 import { ToastComponent } from '../shared/toast/toast.component';
 import { Spot } from '../models/spot';
+import { DataParser } from '../dataParser';
+
 
 @Component({
   encapsulation: ViewEncapsulation.None,
@@ -29,6 +31,8 @@ import { Spot } from '../models/spot';
 
 
 export class MapsComponent implements OnInit {
+
+  @Output() notify: EventEmitter<string> = new EventEmitter<string>();
   //private d3: D3; 
   private parentNativeElement: any;
   
@@ -36,6 +40,8 @@ export class MapsComponent implements OnInit {
   params = {};
   id = [];
   spot = [];
+
+  parser;
 
 
   isLoading = true;
@@ -49,7 +55,7 @@ export class MapsComponent implements OnInit {
   _feature;
   parkingViolations: 0;
   parkingEvents: 0;
-
+  dayStats = Array(1441);
   map;
   mapLayerURL = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
 
@@ -58,10 +64,7 @@ export class MapsComponent implements OnInit {
               private formBuilder: FormBuilder,
               private http: Http,
               public toast: ToastComponent,
-              )
-              
-              {
-              }
+              ){}
 
 
 
@@ -80,14 +83,16 @@ export class MapsComponent implements OnInit {
 
 //定义初始化函数
   ngOnInit() {
+
+
+      this.parser= new DataParser();
+
        if( this.map == null ) this.initMap();
 
 
       d3.select('#leaflet-overlay-pane').selectAll('*').remove();
       this._svg = d3.select('#leaflet-overlay-pane').append("svg");
       this._g = this._svg.append("g").attr("class", "leaflet-zoom-hide");
-
-
 
   }
 
@@ -100,14 +105,15 @@ export class MapsComponent implements OnInit {
       else console.log('map ! null')
 
       //采集该文件的数据
-      this.getMaps(dataFileName);   
-  }  
+    this.getMaps(dataFileName); 
+  }   
+
+
+
 
 //从服务器mean中获取.json提车点的数据
   getMaps(dataFileName) {
-    var self = this;
-
-
+      var self = this;
 
       if( this.map == null )  console.log('map null')
       else console.log('map ! null')
@@ -115,33 +121,10 @@ export class MapsComponent implements OnInit {
     this.mapService.getMaps2(dataFileName).subscribe(
       collection => {
         console.log('ddff');
-
-        //定义上述引入文件中的数据collection为._collection
-        self._collection = JSON.parse(collection._body);
-//        console.log( self._collection );
+        console.log(collection);
 
 
-        for (var i = 0; i < self._collection.features.length; i++) {
-
-            var feature = self._collection.features[i];
-            
-            var coordinates = feature.geometry.coordinates;
-            var bayId = feature.properties.bayId;
-            var streetMarker = feature.properties.streetMarker;
-            var streetName = feature.properties.streetName;
-            var events = feature.properties.sensor;
-
-            var parkingTimeArray = this.parkingSpotTimeArray(self._collection.features[i],20140101);
-            self._collection.features[i].properties.parkingData = parkingTimeArray;
-
-
-        }
-
-
-
-
-
-
+        self._collection = this.parser.parse(collection)
 
 
         var transform = geoTransform({point: function(x,y){
@@ -154,10 +137,12 @@ export class MapsComponent implements OnInit {
                             .data(self._collection.features)
                             .enter().append("path")
                             .attr("d", self._path)
-                            .on( "mouseout" ,  function(d) { console.log(d); },
-                                 "click",  function(d) { console.log(d); },
-                                 "mouseover", function(d) { console.log(d); },
-                            );
+                            .on( "mouseout" ,  function(d) { console.log(d); })
+                            .on( "click" ,  function(d) { 
+                                  console.log(d);
+                                  self.notify.emit(d); 
+                            })
+                            .on( "mouseout" ,  function(d) { console.log(d); });
 
         
 
@@ -166,30 +151,30 @@ export class MapsComponent implements OnInit {
 
 
 
-          d3.select("#leaflet-overlay-pane svg").selectAll("path").attr("class", function (d) {
+        d3.select("#leaflet-overlay-pane svg").selectAll("path").attr("class", function (d) {
 
-                  var key = d.properties.bayId;
-                  var bayStatus;
+                var key = d.properties.bayId;
+                var bayStatus;
 
-                  switch (d.properties.parkingData[minute]) {
-                    case 0:
-                      bayStatus = 'parking-no-monitoring';
-                      break;
-                    case 1:
-                      bayStatus = 'parking-empty';
-                      break;
-                    case 2:
-                      bayStatus = 'parking-taken';
-                      break;
-                    case 3:
-                      bayStatus = 'parking-will-violate';
-                      break;
-                    case 4:
-                      bayStatus = 'parking-in-violation';
-                      break;
-                  }
+                switch (d.properties.parkingData[minute]) {
+                  case 0:
+                    bayStatus = 'parking-no-monitoring';
+                    break;
+                  case 1:
+                    bayStatus = 'parking-empty';
+                    break;
+                  case 2:
+                    bayStatus = 'parking-taken';
+                    break;
+                  case 3:
+                    bayStatus = 'parking-will-violate';
+                    break;
+                  case 4:
+                    bayStatus = 'parking-in-violation';
+                    break;
+                }
 
-                  return bayStatus;
+                return bayStatus;
 
         });
 
@@ -197,131 +182,5 @@ export class MapsComponent implements OnInit {
       }
     );   
   }
-
-
-
-
-
-
-
-
-//判断该车位是那种情况，然后在那种情况里面增加1，即增加停车点的统计
-    addParkingSpotToStats(parkingTimeArray) {
-      let bayStatus;
-      for (var i = 0; i < parkingTimeArray.length; i++) {
-        
-        switch (parkingTimeArray[i]) {
-          case 0:
-          //当判断停车点为超出监控范围
-            bayStatus = 'parkingNoMonitoring';
-            break;
-          case 1:
-          //当判断该停车点为空闲
-            bayStatus = 'parkingEmpty';
-            break;
-          case 2:
-          //当判断该停车点已经停车了
-            bayStatus = 'parkingTaken';
-            break;
-          case 3:
-          //当判断该停车将会违反
-            bayStatus = 'parkingWillViolate';
-            break;
-          case 4:
-          //当判断该停车是合法的
-            bayStatus = 'parkingInViolation';
-            break;
-        }
-
-
-      }
-
-
-    }
-
-
-    parkingSpotTimeArray(feature,currentDate) {
-      //定义日期为当前日期且格式为YYYY-MM-DD
-      var date = 20140101;
-      var dataDate = 20140101;
-
-      var processedData = new Array(1440);
-
-      var i,j;
-      var start,end;
-      var signLookup = {};
-
-      //Loop through add signPlate times, this is when monitoring is happening
-      for (i = 0; i < feature.properties.signPlates.length; i++) {
-
-        //Performance Change
-        //Quicker to pull out hour and times by 60 and then add minute then to use moment
-        start = (feature.properties.signPlates[i].StartTime.substring(0,2)*60) + parseInt(feature.properties.signPlates[i].StartTime.substring(3,5),10);
-        end = (feature.properties.signPlates[i].EndTime.substring(0,2)*60) + parseInt(feature.properties.signPlates[i].EndTime.substring(3,5),10);
-
-        signLookup['S'+feature.properties.signPlates[i].SignPlateId] = feature.properties.signPlates[i].MinutesAllowed;
-
-        for (j = start; j <= end; j++) {
-          processedData[j] = 1;
-        }
-
-
-      }
-
-      //Loop through find each park add data
-      var self = this;
-      for (i = 0; i < feature.properties.sensor.length; i++) {
-
-        //Add to total park events this day
-        this.parkingEvents += 1;
-
-        //Performance Change
-        //Quicker to pull out hour and times by 60 and then add minute then to use moment
-        start = (feature.properties.sensor[i].arrivalDateTime.substring(11,13)*60) + parseInt(feature.properties.sensor[i].arrivalDateTime.substring(14,16),10);
-        end = (feature.properties.sensor[i].departDateTime.substring(11,13)*60) + parseInt(feature.properties.sensor[i].departDateTime.substring(14,16),10);
-
-        var status;
-        if (feature.properties.sensor[i].inViolation) {
-          for ( j = start; j <= end; j++) {
-          processedData[j] = 4;
-          }
-
-          var timeAllowed = signLookup['S'+feature.properties.sensor[i].signPlateId];
-
-          if (end-start > timeAllowed) {
-          for ( j = start; j <= start+ timeAllowed; j++) {
-            processedData[j] = 3;
-          }
-          }
-
-          //Add to total violations this day
-          this.parkingViolations += 1;
-
-
-        } else {
-          for ( j = start; j <= end; j++) {
-          processedData[j] = 2;
-          }
-        }
-      }
-
-
-
-      //Loop through and find gaps fill in with unmonitored/free times
-      for (j = 0; j <= 1440; j++) {
-        //如果输入类型为数据processedData[j]为空的
-        if(typeof processedData[j] === 'undefined'){
-        processedData[j] = 0;
-        }
-      }
-
-      self.addParkingSpotToStats(processedData);
-
-      return processedData;
-    }
-
- 
-
-
 
 }

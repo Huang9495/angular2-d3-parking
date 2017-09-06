732d0154e25323ad73ff76167a50033b6e884040
Kril Allen <15674734251@163.com>
2017-09-06 17:53:38 +0800

    Delete datas.component.spec.ts

--- app/datas/datas.component.spec.ts
+++
@@ -1,9 +0,0 @@
-import { TestBed, async } from '@angular/core/testing';
-import { DatasComponent } from './datas.component';
-
-/*describe('Component: Datas', () => {
-  it('should create an instance', () => {
-    let component = new DatasComponent();
-    expect(component).toBeTruthy();
-  });
-});*/

bfdd9930b64cb753e9dd12abd521c258915a907a
Kril Allen <15674734251@163.com>
2017-09-06 17:53:07 +0800

    Delete parkingspot.ts

--- app/models/parkingspot.ts
+++
@@ -1,5 +0,0 @@
-//变量格式的转换
-export class ParkingSpot {
-   constructor(public bayId: number, public streetMarker: string, public streetName: string,public sensors: any ,public signPlates: any) { 
-   }
-} 
